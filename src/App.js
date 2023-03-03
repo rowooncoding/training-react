bde088ee31a0e0bb1ec9d19efd494d11c9bc13a5
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-03 20:11:33 +0900

    [추가] json 통신으로 데이터 받아오기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
       <h1>The Coins!</h1>
