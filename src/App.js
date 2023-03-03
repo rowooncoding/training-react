12741db1b86ba70749dabb1866f459209e7196c4
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-03 17:19:32 +0900

    [추가] input값 콘솔에 찍어보기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setTodo] = useState("");
+  const onChange = (e) => setTodo(e.target.value);
+  console.log(toDo);
   return (
     <div>
-      <input type="text" placeholder="Write your to do..." />
+      <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder="Write your to do..."
+      />
     </div>
   );
 }
