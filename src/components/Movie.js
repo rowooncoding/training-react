dd1509b33942bd23e6e9880cbee3adb3bf23c2bd
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-04 17:21:13 +0900

    [추가] useParams를 사용하여 동적 Url 제어

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function Movie({ medium_cover_image, title, summary, genres }) {
+export default function Movie({
+  id,
+  medium_cover_image,
+  title,
+  summary,
+  genres,
+}) {
   return (
     <div>
       <img src={medium_cover_image} alt="영화 이미지" />
       <h2>
-        <Link to=".movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
