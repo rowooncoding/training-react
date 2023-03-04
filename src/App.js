26e9fbfb55e5e6328b89de9f59dcddfab9383e41
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-04 16:56:30 +0900

    [추가] 라우터 설치하고 연결하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Movie({ medium_cover_image, title, summary, genres }) {
   return (
     <div>
       <img src={medium_cover_image} alt="영화 이미지" />
-      <h2>{title}</h2>
+      <h2>
+        <Link to=".movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
