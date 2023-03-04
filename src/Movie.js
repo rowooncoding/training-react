8bffa94e17f2fe8695ca7a56cac2cf7c770ae813
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-04 15:31:21 +0900

    [추가] movie 컴포넌트 추가

---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+export default function Movie({ medium_cover_image, title, summary, genres }) {
+  return (
+    <div>
+      <img src={medium_cover_image} alt="영화 이미지" />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
